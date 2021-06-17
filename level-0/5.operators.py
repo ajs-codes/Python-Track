0f87c57f98d6a33364c32c68e96efa314645113d
Sharan Aj <sharanajs2001@gmail.com>
2021-06-17 11:30:59 +0530

    added operators.py

---
+++ level-0/5.operators.py
@@ -0,0 +1,34 @@
+# arithmetic operators in python
+x = 10
+y = 20
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y) #returns remainder
+print(x // y) #floor division returns whole value
+print(x ** y) #exponentiation
+# assignment operators
+x = x + 3
+print(x)
+y = y % 2
+print(y)
+x += 3
+print(x)
+y *= 5
+print(y)
+
+# campartion operators
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+
+# logical operators
+# and or not
+x = 20
+y = 10
+print(x or y)   # returns x value if both are true
+print(x and y) # returns y value if both are true
+print(not(x < y))
+
