39ca9278d7c290fbc1ef9f077de4b9fa27477f90
Sharan Aj <sharanajs2001@gmail.com>
2021-06-18 17:26:30 +0530

    while loop

---
+++ level-2/12.while.py
@@ -0,0 +1,13 @@
+# while syntax
+num = int(input('enter a no : '))
+while num > 0:
+    print('num is positive no',num)
+    num -= 1
+
+# while with else clause
+num = int(input('enter a no : '))
+while num > 0:
+    print('num is positive no',num)
+    num -= 1
+else:
+    print('num is 0 or negative')
